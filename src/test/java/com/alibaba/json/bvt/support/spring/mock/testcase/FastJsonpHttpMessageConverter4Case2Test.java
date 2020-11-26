7c04cb2db1511a42c0238b7032b49607f8a8e226
jmz11 <loveshe1998@126.com>
2020-11-26 15:53:08 -0600

    test flaky

--- src/test/java/com/alibaba/json/bvt/support/spring/mock/testcase/FastJsonpHttpMessageConverter4Case2Test.java
+++ src/test/java/com/alibaba/json/bvt/support/spring/mock/testcase/FastJsonpHttpMessageConverter4Case2Test.java
@@ -86,6 +86,7 @@ public class FastJsonpHttpMessageConverter4Case2Test {
 
     @Test
     public void test1_2() throws Exception {
+        System.out.println("test travis\n");
 
         JSONObject json = new JSONObject();
 
