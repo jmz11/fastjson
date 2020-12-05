2ecff8c8ab14e67548efc247a10fd666f5548163
flakybot2077 <74795189+flakybot2077@users.noreply.github.com>
2020-12-05 16:30:39 -0600

    Use double brackets

--- nondex.sh
+++ nondex.sh
@@ -6,17 +6,17 @@ then
 fi
 nondextests=$(git diff --name-status --diff-filter=AM $COMMIT_RANGE | grep /test/ | sed -e 's;.*test/java/;;' -e 's/.java//' -e 's;/;.;g')
 
-if [ ! -z $nondextests ]
+if [[ ! -z $nondextests ]]
 then
     printf "Running NonDex on tests:\n$nondextests\n"
     nondextests=$($nondextests | tr '\n' ',')
     mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn edu.illinois:nondex-maven-plugin:1.1.2:nondex -DnondexSeed=$RANDOM -DnondexRuns=10 -DfailIfNoTests=false -Dtest=$nondextests
 fi  
-if [ -d ".nondex" ]
+if [[ -d ".nondex" ]]
 then
     flakyTests=$(awk ' !x[$0]++' .nondex/*/failures)
 fi
-if [ ! -z "$flakyTests" ]
+if [[ ! -z "$flakyTests" ]]
 then 
     printf "Found flaky tests:\n$flakyTests\n"
     exit 1 ;
