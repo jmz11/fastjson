5bc7b22137317188e0c7f5e68ee5ae5d6e3dced3
flakybot2077 <74795189+flakybot2077@users.noreply.github.com>
2020-12-05 16:21:23 -0600

    Format nondextests & show NonDex output
    Extra: clean up shell script

--- nondex.sh
+++ nondex.sh
@@ -4,12 +4,13 @@ if [[ -z $TRAVIS_COMMIT_RANGE ]]
 then 
     COMMIT_RANGE=$(git rev-parse origin/master)...$(git rev-parse HEAD)
 fi
-nondextests=$(git diff --name-status --diff-filter=AM $COMMIT_RANGE | grep /test/ | sed -e 's;.*test/java/;;' -e 's/.java//' -e 's;/;.;g' | tr '\n' ','); 
+nondextests=$(git diff --name-status --diff-filter=AM $COMMIT_RANGE | grep /test/ | sed -e 's;.*test/java/;;' -e 's/.java//' -e 's;/;.;g')
 
 if [ ! -z $nondextests ]
 then
-    printf "Running NonDex on tests:\n$nondextests\n" ;
-    mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn edu.illinois:nondex-maven-plugin:1.1.2:nondex -DnondexSeed=$RANDOM -DnondexRuns=10 -DfailIfNoTests=false -Dtest=$nondextests > /dev/null 2>&1 
+    printf "Running NonDex on tests:\n$nondextests\n"
+    nondextests =$($nondextests | tr '\n' ','))
+    mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn edu.illinois:nondex-maven-plugin:1.1.2:nondex -DnondexSeed=$RANDOM -DnondexRuns=10 -DfailIfNoTests=false -Dtest=$nondextests
 fi  
 if [ -d ".nondex" ]
 then
@@ -17,8 +18,8 @@ then
 fi
 if [ ! -z "$flakyTests" ]
 then 
-    printf "Found flaky tests:\n$flakyTests\n" ;
+    printf "Found flaky tests:\n$flakyTests\n"
     exit 1 ;
 else 
-    printf "No flaky tests found.\n" ; 
+    printf "No flaky tests found.\n"
 fi
